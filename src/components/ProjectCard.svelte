2dc05856bc453766857261c3055429f5d0d752f4
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-12-03 19:02:59 +0100

    Improve CSS projects page

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -9,8 +9,8 @@
 	const { link, title, desc, previewUrl }: Props = $props();
 </script>
 
-<a class="m-1 rounded-2xl hover:bg-zinc-800" href={link} target="_blank">
-	<section class="m-2 flex max-w-sm flex-col items-center">
+<a class="m-6 mb-2 mt-0 rounded-2xl hover:bg-zinc-800" href={link} target="_blank">
+	<section class="m-4 flex max-w-sm flex-col items-center">
 		<img class="rounded-2xl" src={previewUrl} alt="Preview of the project" />
 		<h2 class="uppercase">{title}</h2>
 		<p>{desc}</p>
