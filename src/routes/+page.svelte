67a38275feb361cf3657f27cbd8ab67e0d9b59f7
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-12-12 18:14:02 +0100

    Improve frontpage information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,30 @@
-<h1 class="text-center">Bienvenue sur mon Portfolio</h1>
-<p class="m-4 text-center">
-	Bonjour, je m'appelle Maxime Santos et je suis actuellement en recherche d'emploi pour un poste de
-	développeur web <i>front-end</i> / <i>fullstack</i>.
-</p>
+<h1 class="mb-5 text-center">Bienvenue sur mon Portfolio</h1>
+<div class="flex flex-col items-center">
+	<p class="mb-2 max-w-prose">
+		Bonjour, je m'appelle Maxime Santos et je suis actuellement en recherche d'emploi pour un poste
+		de développeur web <i>front-end</i> / <i>fullstack</i>.
+	</p>
 
-<p class="mt-4 text-center">
-	En ré-orientation après un Master en Droit, j'ai obtenu le titre professionnel Développeur Web et
-	Web Mobile, équivalent Bac+2 à l’issue de ma formation à l’école O’Clock, promotion 2023.
-</p>
-<p class=" text-center">
-	Je suis spécialisé en front-end, notamment en React mais aussi Svelte et NextJS.
-</p>
-<p class="text-center">
-	Je reste ouvert à d'autre <i>frameworks</i> comme Angular ou VueJS et travailler en
-	<i>fullstack</i>
-	ou sur du <i>back-end</i> me conviendrait aussi parfaitement.
-</p>
+	<p class="m-2 max-w-prose">
+		En ré-orientation après un Master en Droit, j'ai obtenu le titre professionnel Développeur Web
+		et Web Mobile, équivalent Bac+2, à l’issue de ma formation à l’école O’Clock, promotion 2023.
+	</p>
+	<p class="m-2 max-w-prose">
+		Si je me suis spécialisé en <i>front-end</i> sur React, j'ai aussi été formé en <i>back-end</i> sur
+		Symfony, MySQL, Git et sur la méthodologie Agile.
+	</p>
+	<p class="m-2 max-w-prose">
+		Je reste ouvert à d'autre <i>frameworks</i> comme Svelte, NextJS, Angular ou VueJS et travailler
+		en
+		<i>fullstack</i>
+		ou sur du <i>back-end</i> me conviendrait aussi parfaitement.
+	</p>
 
-<p class="m-4 text-center">
-	Si mon profil vous intéresse, que vous souhaitez un CV complet ou me poser plus de
-	questions,&nbsp;<a class="italic hover:underline" href="/contact">contactez moi par mail</a>&nbsp!
-</p>
+	<p class="m-2 max-w-prose">
+		Rigoureux, à l'écoute et avec une forte envie d'apprendre, si mon profil vous intéresse et que
+		vous souhaitez un CV complet ou me poser plus de questions,&nbsp;<a
+			class="italic hover:underline"
+			href="/contact">contactez moi par mail</a
+		>&nbsp!
+	</p>
+</div>
